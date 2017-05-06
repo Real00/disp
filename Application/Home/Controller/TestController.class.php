5062f6e3c76f4d30293029d56af95054fbee06ec
Real00 <junrui5500@qq.com>
2017-05-06 15:40:53 +0800

    修正代码

--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 namespace Home\Controller;
 use Think\Controller;
@@ -13,19 +12,4 @@ class TestController extends Controller{
         $this->display();
     }
 }
-=======
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class TestController extends Controller{
-    public function index(){
-        $this->display();
-    }
-    public function map($mp){
-        $s = explode(",",$mp);
-        $this->assign("longitude",$s[0]);
-        $this->assign("latitude",$s[1]);
-        $this->display();
-    }
-}
->>>>>>> Real00/master
+?>
--- Application/Runtime/Cache/Home/8add4cccb19322283ad1a0f83187c896.php
+++ Application/Runtime/Cache/Home/8add4cccb19322283ad1a0f83187c896.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
 <html>
 <head>
@@ -18,31 +17,10 @@
 	// 百度地图API功能
 	var map = new BMap.Map("allmap");    // 创建Map实例
 	map.centerAndZoom(new BMap.Point(<?php echo ($longitude); ?>, <?php echo ($latitude); ?>), 20);  // 初始化地图,设置中心点坐标和地图级别
+	var point = new BMap.Point(<?php echo ($longitude); ?>, <?php echo ($latitude); ?>);//添加标注点
+	var marker = new BMap.Marker(point);//将标注点转化
+	map.addOverlay(marker);  //将标注点添加到地图上
 	map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
 	map.setCurrentCity("广州");          // 设置地图显示的城市 此项是必须设置的
 	map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
-=======
-<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-	<style type="text/css">
-	body, html,#allmap {width: 100%;height: 100%;overflow: hidden;margin:0;font-family:"微软雅黑";}
-	</style>
-	<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=ixI3xWKUlShFfd93sYW4uS2kMf5ry0RA"></script>
-	<title>地图展示</title>
-</head>
-<body>
-	<div id="allmap"></div>
-</body>
-</html>
-<script type="text/javascript">
-	// 百度地图API功能
-	var map = new BMap.Map("allmap");    // 创建Map实例
-	map.centerAndZoom(new BMap.Point(<?php echo ($longitude); ?>, <?php echo ($latitude); ?>), 20);  // 初始化地图,设置中心点坐标和地图级别
-	map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
-	map.setCurrentCity("广州");          // 设置地图显示的城市 此项是必须设置的
-	map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
->>>>>>> Real00/master
 </script>
